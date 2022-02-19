a7877385236082732b98be82d075d3b107f063b0
Cavonstavant <constant.vigneron@protonmail.com>
2022-02-19 21:29:27 +0100

    [+] my_strchr

--- src/my_strrchr.asm
+++ src/my_strrchr.asm
@@ -13,19 +13,19 @@ strrchr:
     add rcx, rax
 
 loop:
-    cmp [rdi + rcx], sil; compare str[0 + rcx] with the c
-    je found
-    cmp byte[rdi + rcx], 0; check if we reached the end of the string
-    dec rcx ; decrease the counter
+    cmp byte[rdi], sil; compare str[0 + rcx] with the c
+    je found ; if yes jump
+    cmp byte[rdi], 0; check if we reached the end of the string
+    je not_found ; if yes jump
+    dec rdi; decrease the counter
     jmp loop
 
 found:
-    add rdi, rcx ; add the offset to the string
     mov rax, rdi ; return the pointer to the found character
     leave
     ret
 
 not_found:
-    mov rax,0 ;  return NULL
+    mov rax, 0 ;  return NULL
     leave
     ret

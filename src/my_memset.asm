a0055bcc0b363901b671b2de01be4ee5b5160188
Cavonstavant <constant.vigneron@protonmail.com>
2022-02-20 00:35:30 +0100

    [+] commented memset

--- src/my_memset.asm
+++ src/my_memset.asm
@@ -5,12 +5,12 @@ section .text
 memset:
     enter 0, 0
 loop:
-    cmp rcx, 0
-    je end
-    mov [rdi], sil
-    inc rdi
-    dec rcx
-    jmp loop
+    cmp rcx, 0 ; check if we are done
+    je end ; if yes jump
+    mov [rdi], sil ; else write to memory
+    inc rdi ; increment offset
+    dec rcx ; decrement count
+    jmp loop ; loop
 
 end:
     mov rax, rdi

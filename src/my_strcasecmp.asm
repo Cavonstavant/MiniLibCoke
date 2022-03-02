d615799dcb848428f234650a0f23cf89dcbc8e67
Cavonstavant <constant.vigneron@protonmail.com>
2022-03-02 21:10:34 +0100

    [+] strcasecmp placeholder

--- src/my_strcasecmp.asm
+++ src/my_strcasecmp.asm
@@ -4,20 +4,4 @@ extern strcmp
 section .text
 
 strcasecmp:
-    enter 0, 0
-setup:
-    jmp strcmp wrt ..plt
-    leave
     ret
-
-; loop:
-;     cmp rdx, 0 ; check if we are done
-;     je end ; if yes jump
-;     mov r8b, byte[rsi + rdx] ; cpy byte from src to temp register
-;     mov byte[rdi + rdx], r8b ; cpy temp byte to dest
-;     dec rdx ; decrement counter
-;     jmp loop ; jump to loop
-; end:
-;     mov rax, rdi
-;     leave
-;     ret

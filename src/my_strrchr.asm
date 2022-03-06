56921159c65f3b5bb8669d2c3c6669ef106d65c5
Cavonstavant <constant.vigneron@protonmail.com>
2022-03-06 18:51:28 +0100

    fix strrchr

--- src/my_strrchr.asm
+++ src/my_strrchr.asm
@@ -9,16 +9,15 @@ rindex:
 strrchr:
     enter 0,0
     cmp rdi, 0 ; check if the string is NULL / empty
-    jz not_found
+    je not_found
     call strlen wrt ..plt
     mov r8, rax
-    sub r8, 1
 
 loop:
+    cmp byte[rdi + r8], sil; compare str[0 + r8] with the c
+    je found ; if yes jump
     cmp byte[rdi + r8], 0; check if we reached the end of the string
     je not_found ; if yes jump
-    cmp byte[rdi + r8], sil; compare str[0 + rcx] with the c
-    je found ; if yes jump
     dec r8; decrease the counter
     jmp loop
 
